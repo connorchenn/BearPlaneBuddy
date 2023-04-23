8c6eb6923533933e1fcd7a12e53d876b59b854a7
Vishnu Suresh <vishnusuresh@berkeley.edu>
2023-04-23 13:30:09 -0700

    Making progress on Firebase backend.

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+var hbs = require('handlebars');
+const admin = require('firebase-admin');
+
+const app = express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+
+var serviceAccount = require("./bearplanebuddy-firebase-adminsdk-2zk04-31e4f40d76.json");
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "https://bearplanebuddy.firebaseio.com"
+});
+
+async function getFirestore(){
+    const firestore_con  = await admin.firestore();
+    const writeResult = firestore_con.collection('sample').doc('sample_doc').get().then(doc => {
+    if (!doc.exists) { console.log('No such document!'); }
+    else {return doc.data();}})
+    .catch(err => { console.log('Error getting document', err);});
+    return writeResult
+}
+
+app.get('/',async (request,response) =>{
+    var db_result = await getFirestore();
+    response.render('index',{db_result});
+    });
+    exports.app = functions.https.onRequest(app);
