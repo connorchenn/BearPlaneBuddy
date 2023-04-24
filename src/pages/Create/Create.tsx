0bb479672ec9cfd7bb9bae5ba186d3b7274a2a2b
Connor <connorchen@berkeley.edu>
2023-04-23 17:37:12 -0700

    a

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,3 +1,38 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import useAuth from 'contexts/Auth/useAuth';
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+  apiKey: 'AIzaSyAp1ZJXQmr1RQnEE-aJisME3CsiffyaZX0',
+  authDomain: 'bearplanebuddy.firebaseapp.com',
+  projectId: 'bearplanebuddy',
+  storageBucket: 'bearplanebuddy.appspot.com',
+  messagingSenderId: '1095288640396',
+  appId: '1:1095288640396:web:46fb14569653264d4a7ca9',
+  measurementId: 'G-T0DVLEC2P3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
 export default function Create() {
+  const { user} = useAuth();
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      name: user?.displayName,
+      email: user?.email,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
   return <div></div>;
 }
